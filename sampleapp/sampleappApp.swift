4447ab4217d6d57bf0a9685eab3a9a3c8b55575b
Ricarlo Silva <>
2021-10-30 01:16:15 -0300

    first commit

---
+++ sampleapp/sampleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleappApp.swift
+//  sampleapp
+//
+//  Created by Ricarlo Silva on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct sampleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
